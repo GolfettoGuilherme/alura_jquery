6fac943e2d09cf2044aa6ea09b51f249b50ede5a
Guilherme Golfetto <Guilherme Golfetto>
2016-11-09 00:11:30 -0200

    iniciado segundo modulo curso jQuery

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,6 @@
+$('#botao-placar').click(mostraPlacar);
+
+
 function inserePlacar(){
 	//para encontrar um elemento dentro de outro
 	var corpoTabela = $(".placar").find("tbody");
@@ -9,6 +12,17 @@ function inserePlacar(){
 	linha.find('.botao-remover').click(removeLinha); //atribui o evento de click 
 
 	corpoTabela.prepend(linha);
+
+	$('.placar').slideDown(500);
+	scrollPlacar();
+
+}
+
+function scrollPlacar(){
+	var posicaoPlacar = $('.placar').offset().top;
+	$('body').animate({
+		scrollTop: posicaoPlacar + "px"
+	},1000);
 }
 
 function novaLinha(usuario,numPalavras){
@@ -31,5 +45,21 @@ function novaLinha(usuario,numPalavras){
 
 function removeLinha(){
 	event.preventDefault();	//o event é o evento padrão que aconteceria se fosse clicado, usando isso, não acontece o redirecionamento da tag <a>
-	$(this).parent().parent().remove(); //this = elemento clicado, agora ele tem os poderes jQuery, parent = o pai do elemento
+	var linha = $(this).parent().parent();
+	// .remove(); //this = elemento clicado, agora ele tem os poderes jQuery, parent = o pai do elemento
+	linha.fadeOut(1000); //animação para dar displaynome
+	setTimeout(function(){
+		linha.remove(); //remover depois do tempo para o fadeout
+	},1000);
+}
+
+function mostraPlacar(){
+	// $('.placar').show();  //$('.placar').css('display','block');
+	// $('.placar').hide();  //$('.placar').css('display','none');
+	// $('.placar').toggle(); //mostra e esconde, sem a necessidade de fazer um if
+	// $('.placar').slideDown(2000); //animação para aparecer
+	// $('.placar').slideUp(2000); //animação para sumir
+	// .stop - para o que esta fazendo antes para fazer a proxima
+	$('.placar').stop().slideToggle(600); 
+
 }
